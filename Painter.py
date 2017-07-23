bca6706622e9817276f078d5f1b1bc5cd972bdfe
DavidSinz <david.sinz@stud.uni-regensburg.de>
2017-07-23 15:39:10 +0200

    Undo funktion eingebaut

--- Painter.py
+++ Painter.py
@@ -5,9 +5,12 @@ from Tkinter import *
 from PIL import ImageGrab
 
 class Button(object):
+	root, painter = None, None
 	width, height, padx, pady = 0, 0, 0, 0
 
-	def __init__(self, root, width, height, padx, pady):
+	def __init__(self, painter, root, width, height, padx, pady):
+		self.root = root
+		self.painter = painter
 		self.width = width
 		self.height = height
 		self.padx = padx
@@ -15,8 +18,7 @@ class Button(object):
 
 class ColorButton(Button):
 	def __init__(self, painter, root, width, height, padx, pady, color):
-		Button.__init__(self, root, width, height, padx, pady)
-		self.painter = painter
+		Button.__init__(self, painter, root, width, height, padx, pady)
 		self.color = color
 		self.TkBut = Tkinter.Button(root, command=self.button_event, width=width, height=height, bg=color)
 		self.TkBut.pack(padx=padx, pady=pady, side=LEFT)
@@ -26,8 +28,7 @@ class ColorButton(Button):
 
 class ToolButton(Button):
 	def __init__(self, painter, drawing_area, root, width, height, padx, pady, tool, img):
-		Button.__init__(self, root, width, height, padx, pady)
-		self.painter = painter
+		Button.__init__(self, painter, root, width, height, padx, pady)
 		self.drawing_area = drawing_area
 		self.tool = tool
 		self.TkBut = Tkinter.Button(root, command=self.button_event, width=width, height=height, bg="white")
@@ -43,8 +44,7 @@ class ToolButton(Button):
 
 class LineWidthButton(Button):
 	def __init__(self, painter, root, width, height, padx, pady, lineWidth):
-		Button.__init__(self, root, width, height, padx, pady)
-		self.painter = painter
+		Button.__init__(self, painter, root, width, height, padx, pady)
 		self.lineWidth = lineWidth
 		self.TkBut = Tkinter.Button(root, text=lineWidth, command=self.button_event, width=width, height=height, bg="white")
 		self.TkBut.pack(padx=padx, pady=pady, side=LEFT)
@@ -58,12 +58,11 @@ class LineWidthButton(Button):
 			self.painter.pencil_line_width = 20
 
 class SaveButton(Button):
-	i = 1
+	saveIndex = 1
 	
-	def __init__(self, drawing_area, root, width, height, padx, pady):
-		Button.__init__(self, root, width, height, padx, pady)
+	def __init__(self, painter, drawing_area, root, width, height, padx, pady):
+		Button.__init__(self, painter, root, width, height, padx, pady)
 		self.drawing_area = drawing_area
-		self.root = root
 		self.TkBut = Tkinter.Button(root, text="speichern", command=self.button_event, width=width, height=height, bg="white")
 		self.TkBut.pack(padx=padx, pady=pady, side=LEFT)
 	
@@ -72,8 +71,25 @@ class SaveButton(Button):
 		y=self.root.winfo_rooty()+self.drawing_area.winfo_y()
 		x1=x+self.drawing_area.winfo_width()
 		y1=y+self.drawing_area.winfo_height()
-		ImageGrab.grab().crop((x,y,x1,y1)).save("save" + str(self.i) + ".jpg")
-		self.i = self.i + 1
+		ImageGrab.grab().crop((x,y,x1,y1)).save("save" + str(self.saveIndex) + ".jpg")
+		self.saveIndex = self.saveIndex + 1
+
+class UndoButton(Button):
+	def __init__(self, painter, drawing_area, root, width, height, padx, pady):
+		Button.__init__(self, painter, root, width, height, padx, pady)
+		self.drawing_area = drawing_area
+		self.TkBut = Tkinter.Button(root, text="Undo", command=self.button_event, width=width, height=height, bg="white")
+		self.TkBut.pack(padx=padx, pady=pady, side=LEFT)
+
+	def button_event(self):
+		if len(self.painter.cv_elements) > 0:
+			if type(self.painter.cv_elements[len(self.painter.cv_elements) - 1]) is list:
+				el_list = self.painter.cv_elements[len(self.painter.cv_elements) - 1]
+				for i in range(len(el_list)):
+					self.drawing_area.delete(el_list[i])
+			else:
+				self.drawing_area.delete(self.painter.cv_elements[len(self.painter.cv_elements) - 1])
+			del self.painter.cv_elements[-1]
 
 class Painter:
     drawing_tool = "pencil"
@@ -81,13 +97,14 @@ class Painter:
     pencil_line_width = 1
     but_pressed = False
     x_pos, y_pos = None, None
-    d = []
 
     x1, y1, x2, y2 = None, None, None, None
 
     tool_names = ["pencil", "line", "circle", "rect", "eraser", "delete"]
     color_codes = ["#000000", "#808080", "#C0C0C0", "#FFFFFF", "#800000", "#FF0000", "#808000", "#FFFF00", "#008000", "#00FF00", "#008080", "#00FFFF", "#000080", "#0000FF", "#800080", "#FF00FF"]
     line_widths = ["duenn", "mittel", "dick"]
+    cv_elements = []
+    cv_el_list = []
 
     def button_pressed(self, event=None):
         self.but_pressed = True
@@ -113,46 +130,40 @@ class Painter:
     def pencil_draw(self, event= None):
         if self.but_pressed:
             if (self.x_pos and self.y_pos) != None:
-                event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y,width=self.pencil_line_width,smooth=True,fill=self.pencil_color)
-
+                self.cv_el_list.append(event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y,width=self.pencil_line_width,smooth=True,fill=self.pencil_color))
             self.x_pos, self.y_pos = event.x, event.y
+        if not self.but_pressed and self.cv_el_list != []:
+            self.cv_elements.append(self.cv_el_list)
+            self.cv_el_list = []
 
     def line_draw(self,event=None):
         if (self.x1,self.y1,self.x2,self.y2) != None:
-            self.d.append(event.widget.create_line(self.x1,self.y1,self.x2,self.y2,width=self.pencil_line_width, smooth = True, fill=self.pencil_color))
+            self.cv_elements.append(event.widget.create_line(self.x1,self.y1,self.x2,self.y2,width=self.pencil_line_width, smooth = True, fill=self.pencil_color))
 
     def circle_draw(self, event=None):
         if (self.x1, self.y1, self.x2, self.y2) != None:
-            self.d.append(event.widget.create_oval(self.x1, self.y1, self.x2, self.y2, width=30, fill=self.pencil_color, outline=self.pencil_color))
+            self.cv_elements.append(event.widget.create_oval(self.x1, self.y1, self.x2, self.y2, width=30, fill=self.pencil_color, outline=self.pencil_color))
 
     def rectangle_draw(self, event=None):
         if (self.x1, self.y1, self.x2, self.y2) != None:
-            self.d.append(event.widget.create_rectangle(self.x1, self.y1, self.x2, self.y2, width = 30, fill=self.pencil_color, outline=self.pencil_color))
+            self.cv_elements.append(event.widget.create_rectangle(self.x1, self.y1, self.x2, self.y2, width = 30, fill=self.pencil_color, outline=self.pencil_color))
 
     def eraser_draw(self, event=None):
         if self.but_pressed:
             if (self.x_pos and self.y_pos) != None:
-                event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y,width = 30,smooth=True,fill="white")
-
+                self.cv_el_list.append(event.widget.create_line(self.x_pos, self.y_pos, event.x, event.y,width = 30,smooth=True,fill="white"))
             self.x_pos, self.y_pos = event.x, event.y
-
-    x = 1
+        if not self.but_pressed and self.cv_el_list != []:
+            self.cv_elements.append(self.cv_el_list)
+            self.cv_el_list = []
 
     def init_buttons(self, drawing_area):
         for i in range(6):
             ToolButton(self, drawing_area, root, 50, 50, 2, 2, self.tool_names[i], ImageTk.PhotoImage(file=self.tool_names[i]+".png"))
         for i in range(3):
             LineWidthButton(self, root, 5, 3, 2, 2, self.line_widths[i])
-        SaveButton(drawing_area, root, 5, 3, 2, 2)
-        def func_undo():
-            drawing_area.delete(self.d[len(self.d) - self.x])
-            self.x = self.x +1
-        def func_redo():
-            print str(self.d[len(self.d) - 1])
-        undo = Tkinter.Button(root, text="undo", command=func_undo, width=5, height=3, bg="white")
-        undo.pack(padx=2, pady=2, side=LEFT)
-        redo = Tkinter.Button(root, text="redo", command=func_redo, width=5, height=3, bg="white")
-        redo.pack(padx=2, pady=2, side=LEFT)
+        SaveButton(self, drawing_area, root, 5, 3, 2, 2)
+        UndoButton(self, drawing_area, root, 5, 3, 2, 2)
         for i in range(16):
             ColorButton(self, root, 2, 1, 2, 2, self.color_codes[i])
 
